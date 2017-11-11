e922a8421e669928c50ae0af73b6acbb780099a7
Leonardo Matos Pessoa <leonardom.pessoa@gmail.com>
2017-11-11 15:14:45 -0200

    Initial Commit

--- src/test/java/com/labfolder/keywordfrequencyservice/business/KeywordAnalyzerTest.java
+++ src/test/java/com/labfolder/keywordfrequencyservice/business/KeywordAnalyzerTest.java
@@ -11,9 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,13 +30,21 @@ public class KeywordAnalyzerTest {
 
         String keyword = "Word";
         String entryStr = "Word Words Wor word";
+        Set expectedSet = new HashSet();
+
+        expectedSet.add("Words");
+        expectedSet.add("word");
+        expectedSet.add( "Wor");
+
         InputStream entry = new ByteArrayInputStream(entryStr.getBytes(StandardCharsets.UTF_8.name()));
 
         KeywordFrequency keywordFrequency = keywordAnalyzer.analyze(entry, keyword);
 
         assertEquals(1, keywordFrequency.getFrequency());
         assertEquals(3, keywordFrequency.getSimilarWords().length);
-        assertArrayEquals(new String[]{"Words", "Wor", "word"}, keywordFrequency.getSimilarWords());
+        for(String similar : keywordFrequency.getSimilarWords()) {
+            assertTrue(expectedSet.contains(similar));
+        }
 
     }
 
@@ -72,13 +83,22 @@ public class KeywordAnalyzerTest {
 
         String keyword = "word";
         String entryStr = "Word wor words";
+        Set expectedSet = new HashSet();
+
+        expectedSet.add("Word");
+        expectedSet.add("words");
+        expectedSet.add( "wor");
+
         InputStream entry = new ByteArrayInputStream(entryStr.getBytes(StandardCharsets.UTF_8.name()));
 
         KeywordFrequency keywordFrequency = keywordAnalyzer.analyze(entry, keyword);
 
         assertEquals(0, keywordFrequency.getFrequency());
         assertEquals(3, keywordFrequency.getSimilarWords().length);
-        assertArrayEquals(new String[]{"Word", "words", "wor"}, keywordFrequency.getSimilarWords());
+        for(String similar : keywordFrequency.getSimilarWords()) {
+            assertTrue(expectedSet.contains(similar));
+        }
+
 
     }
 }
