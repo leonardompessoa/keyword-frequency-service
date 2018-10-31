02c791dea2c96aa1e8af0e140149665c38650e1e
Leonardo Matos Pessoa <leonardom.pessoa@gmail.com>
2018-10-31 14:58:19 +0100

    Alterado para utilizar Streams de forma mais correta

--- src/main/java/com/labfolder/business/KeywordAnalyzerImpl.java
+++ src/main/java/com/labfolder/business/KeywordAnalyzerImpl.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service("keywordAnalyzer")
 public class KeywordAnalyzerImpl implements KeywordAnalyzer {
@@ -17,28 +20,33 @@ public class KeywordAnalyzerImpl implements KeywordAnalyzer {
     @Override
     public KeywordFrequency analyze(final InputStream noteEntry, final String keyword) {
 
-        Pattern pattern = Pattern.compile(keyword);
-        final int [] count = {0};
-        Set<String> similarWords = new HashSet<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(noteEntry));
-        reader.lines().forEach(
-                (line) -> {
-                    String [] words = line.trim().split(("\\s+"));
-                    for(String word : words) {
-                        word = word.replaceAll("\\.","");
-                        word = word.replaceAll("\\,","");
-                        word = word.replaceAll("\\!","");
-                        word = word.replaceAll("\\?","");
-                        if(word.equals(keyword)){
-                            count[0]++;
-                        } else if(StringUtils.getLevenshteinDistance(keyword, word) <= 1 ) {
-                            similarWords.add(word);
-                        }
-                    }
-                }
-        );
-        String [] similar = similarWords.toArray(new String[similarWords.size()]);
-        return new KeywordFrequency(count[0], similar);
+        Function<String, Stream<String>> splitIntoWords = line -> {
+            line = line.trim()
+                    .replaceAll("\\.", "")
+                    .replaceAll("\\,", "")
+                    .replaceAll("\\!", "")
+                    .replaceAll("\\?", "");
+            return Pattern.compile(" ").splitAsStream(line);
+        };
+
+        List<String> listOfWords = reader.lines()
+                .flatMap(splitIntoWords)
+                .collect(Collectors.toList());
+
+        Set<String> similarWords = listOfWords.stream()
+                .filter(word -> !word.equals(keyword))
+                .filter(word -> StringUtils.getLevenshteinDistance(keyword, word) <= 1)
+                .collect(Collectors.toSet());
+
+        Long equalWords = listOfWords.stream()
+                .flatMap(splitIntoWords)
+                .filter(word -> word.equals(keyword))
+                .collect(Collectors.counting());
+
+
+        String[] similar = similarWords.toArray(new String[similarWords.size()]);
+        return new KeywordFrequency(equalWords.intValue(), similar);
     }
 
 }
